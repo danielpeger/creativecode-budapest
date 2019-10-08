d7dc110da00e3da882ead359c3fb412a5a629731
danielpeger <pegerdaniel@gmail.com>
2019-10-08 21:59:25 +0200

    photso field, cloudinary

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,6 @@
 import CMS from 'netlify-cms-app'
-
+import cloudinary from 'netlify-cms-media-library-cloudinary';
 import EventPagePreview from './preview-templates/EventPagePreview'
 
-CMS.registerPreviewTemplate('event', EventPagePreview)
+CMS.registerMediaLibrary(cloudinary);
+CMS.registerPreviewTemplate('event', EventPagePreview);
