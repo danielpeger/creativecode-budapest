d883a8894a4d5825249e3f777a3dcbfb19235073
danielpeger <pegerdaniel@gmail.com>
2020-01-07 17:49:31 +0100

    podcast card

--- src/components/PodcastEpisode.js
+++ src/components/PodcastEpisode.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import { Link } from "gatsby"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import DateString from "./DateString"
 import MarkdownToHtml from "../utils/MarkdownToHtml"
+import truncateString from "../utils/truncateString"
 import Autolinker from 'autolinker'
 import { Label } from "./GlobalStyle"
 import Button from "../components/Button"
@@ -11,42 +12,68 @@ import Audio from './Audio'
 
 const PodcastEpisodeElement = styled.div`
   display: contents;
+`;
 
-  > img {
-    min-width: 100%;
-    min-height: 100%;
-    width: 100%;
-    display: block;
-    object-fit: cover;
-    grid-column: 1 / 4;
-  }
+const EpisodeImage = styled.img`
+  min-width: 100%;
+  min-height: 100%;
+  width: 100%;
+  display: block;
+  object-fit: cover;
+  grid-column: 1 / 3;
+`;
+
+const EpisodeCard = styled.div`
+  display: contents;
 `;
 
 const Info = styled.div`
-  grid-column: 4 / -1;
-`;
-
-const Player = styled(Audio)`
-  grid-column: 4 / -1;
+  grid-column: 3 / -1;
+  
 `;
 
 const AllEpisodesButton = styled(Button)`
   grid-column: 1 / -1;
 `;
 
+const ShowMore = styled.a`
+  text-decoration: underline;
+  cursor: pointer;
+
+  ${props => props.hide && css`
+    display: none;
+  `}
+`;
+
 const PodcastEpisode = ({ date, frontpage, title, shownotes, imgSrc, src }) => {
+  const truncatedShownotes = truncateString(shownotes, 140);
+  const truncatedLinkedShownotes = Autolinker.link(truncatedShownotes);
   const linkedShownotes = Autolinker.link(shownotes);
+  const descriptionElement = useRef(null);
+  const [descriptionExpanded, setDescriptionExpanded] = useState(false);
+  function expandShownotes() {
+    descriptionElement.current.innerHTML = MarkdownToHtml(linkedShownotes);
+    setDescriptionExpanded(true);
+  }
   return (
     <PodcastEpisodeElement noseparator>
-      <img src={imgSrc} alt="Podcast episode thumbnail" />
-      <Info>
-        <Label>{frontpage && "Latest episode ("}<DateString date={date} format="short"/>{frontpage && ")"}</Label>
-        <h2>{title}</h2>
-        <div
-          dangerouslySetInnerHTML={{ __html: MarkdownToHtml(linkedShownotes) }}
-        ></div>
-      </Info>
-      <Player src={src} />
+      <EpisodeCard>
+        <EpisodeImage src={imgSrc} alt="Podcast episode thumbnail" />
+        <Info>
+          <Label>{frontpage && "Latest episode ("}<DateString date={date} format="short"/>{frontpage && ")"}</Label>
+          <h2>{title}</h2>
+          <p>
+            <span
+              ref={descriptionElement}
+              dangerouslySetInnerHTML={{ __html: MarkdownToHtml(truncatedLinkedShownotes) }}
+            ></span>
+            <ShowMore onClick={expandShownotes} hide={descriptionExpanded}>
+                Show more...
+            </ShowMore>
+          </p>
+        </Info>
+      </EpisodeCard>
+      <Audio src={src} />
       {frontpage &&
         <AllEpisodesButton>
           <Link to="/podcast">All episodes <ArrowIcon /></Link>
